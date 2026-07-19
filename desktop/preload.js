d4893c4a0d5a3ac5b7db7c077eb51017d44c26f5
fkdyall <ptwessling@gmail.com>
2026-07-19 09:20:07 -0500

    Add isolated desktop preload bridge

---
+++ desktop/preload.js
@@ -0,0 +1,15 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('silentP', {
+  isNative: true,
+  openProfile: (profile) => ipcRenderer.invoke('browser:open', profile),
+  closeProfile: () => ipcRenderer.invoke('browser:close'),
+  navigate: (url) => ipcRenderer.invoke('browser:navigate', url),
+  command: (command) => ipcRenderer.send('browser:command', command),
+  toggleDesktop: () => ipcRenderer.invoke('browser:toggle-desktop'),
+  onBrowserState: (callback) => {
+    const handler = (_event, state) => callback(state);
+    ipcRenderer.on('browser:state', handler);
+    return () => ipcRenderer.removeListener('browser:state', handler);
+  }
+});
